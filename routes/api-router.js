3c2d713f87970bf66fb529028e3163640ec9c790
Tami <kazehare.t@gmail.com>
2024-08-01 13:40:39 +0100

    get users available

--- routes/api-router.js
+++ routes/api-router.js
@@ -2,6 +2,7 @@ const { getEndpoints } = require("../controllers/api-controllers");
 const articlesRouter = require("./articles-router");
 const commentsRouter = require("./comments-router");
 const topicsRouter = require("./topics-router");
+const usersRouter = require("./users-router");
 
 const apiRouter = require("express").Router();
 
@@ -12,5 +13,7 @@ apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/articles", articlesRouter);
 
 apiRouter.use("/comments", commentsRouter);
+
+apiRouter.use("/users", usersRouter);
 
 module.exports = apiRouter;

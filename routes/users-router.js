db2f0d8d1c04b76395b492f319747fd29b237ca9
Tami <kazehare.t@gmail.com>
2024-08-05 14:17:44 +0100

    get username available

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,7 +1,9 @@
-const { getUsers } = require("../controllers/users-controllers");
+const { getUsers, getUser } = require("../controllers/users-controllers");
 
 const usersRouter = require("express").Router();
 
 usersRouter.route("/").get(getUsers);
+
+usersRouter.route("/:username").get(getUser);
 
 module.exports = usersRouter;
